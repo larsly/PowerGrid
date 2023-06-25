4b28005b767c61b690cd317e86a5c1d7f4a1b435
larsly <larissa.bierly@gmail.com>
2023-06-25 16:27:26 -0400

    started function for favorites in local storage

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// assuming that we have a favorite button next to our populated list items
+// function to loop through our list items and add a click event listener to each favorite button
+// needs to be workshopped but its a starting point
+var btnEls = document.getElementById("favorite");
+var textEls = document.getElementById("location");
+for (var i = 0; i < btnEls.length; i++) {
+    textEls[i].innerText = JSON.parse(localStorage.getItem(btnEls[i].parentElement.id));
+    btnEls[i].addEventListener("click", function () {
+      var saveText = $(this).siblings("#location").val();
+      localStorage.setItem(this.parentElement.id, JSON.stringify(saveText));
+    });
+  }
