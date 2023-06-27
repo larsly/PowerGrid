eacd164e7788edced04def860101a9cfd69fd595
James Matthew Haynes <jamesmatthewhaynes@jamess-imac.myfiosgateway.com>
2023-06-27 17:31:43 -0400

    connected NREL APIand made fetch request

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,17 @@
 $(document).foundation()
 
+//fetch API query for charging stations
+var evQuery = "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=YuxEi5gp0aq25h7DrlIY1TjV3LyXZI9dxAVRt5oX&location=1617+Cole+Blvd+Golden+CO&fuel_type=ELEC&access=public&radius=15.0&ev_network=all&limit=5"
+/* params to use:
+    access=public
+    fuel_type=ELEC
+    radius=15.0       //15 mile radius
+    ev_network=all
+  to-do-list:
+   -the location= parameter needs to be filled in dynamically whenever the user submits info with the button
+   -we could store that input in a variable and concatenate that into the url
+   -one of our previous activities did something similar
+*/
 
 // set up our event listener for search button. Need to write function handleSearchFormSubmit
 var searchForm = document.querySelector('#search');
@@ -17,3 +29,15 @@ for (var i = 0; i < btnEls.length; i++) {
       localStorage.setItem(this.parentElement.id, JSON.stringify(saveText));
     });
   }
+
+
+//just here so that we can look in the console and see what details we can access
+fetch(evQuery)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  })
+
+
